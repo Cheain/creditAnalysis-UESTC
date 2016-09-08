0bc52c1f4dd7459d44ddd5dd7c252f632167934a
Cheain <842217191@qq.com>
2016-09-09 00:38:21 +0800

    第二版
    增加验证登陆是否成功；增加读取/写入本地密码文件功能；增加自动更新功能；代码结构改变；加入windows程序版本信息；

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import requests
@@ -41,7 +42,6 @@ class student(object):
 
     def post(self, url, postInfo):
         response = self.session.post(url=url, headers=self.my_header, data=postInfo, timeout=30)
-        # , cert = 'F:\Programs\Class-3-Public-Primary-Certification-Authority.pem')
         return response
 
     def login(self):
@@ -54,13 +54,10 @@ class student(object):
         self.rmShown = soup.find('input', {'name': 'rmShown'})['value']
         postInfo = {'username': self.username, 'password': self.password, 'lt': self.lt, 'dllt': self.dllt,
                     'execution': self.execution, '_eventId': self._eventId, 'rmShown': self.rmShown}
-        try:
-            self.post(loginURL, postInfo)
-        except Exception as e:
-            print('登陆失败，错误提示{}'.format(e))
-
-            # response content: {"r":0, "msg": "\u767b\u9646\u6210\u529f" }
-            # print(self._session.get('http://portal.uestc.edu.cn/index.portal').text)
+        response = self.post(loginURL, postInfo)
+        if response.status_code == 200:
+            print('登陆失败，学号或密码错误，请重试\n')
+            main()
 
     def getGrade(self, term):
         print('\n当前为第{}学期:'.format(term))
@@ -80,7 +77,7 @@ class student(object):
         # self.creditCount(gradeSoup)
 
     def outPut(self, gradeSoup):
-        x = PrettyTable(['学期', '课程代码', '课程名称', '课程种类', '学分', '正考成绩', '补考成绩', '最终成绩', '绩点'])
+        courseTable = PrettyTable(['学期', '课程代码', '课程名称', '课程种类', '学分', '正考成绩', '补考成绩', '最终成绩', '绩点'])
         for i in range(1, len(gradeSoup)):
             term = gradeSoup[i].contents[1].contents[0].strip()
             courseCode = gradeSoup[i].contents[3].contents[0].strip()
@@ -92,19 +89,21 @@ class student(object):
             secondGrade = gradeSoup[i].contents[14].contents[0].strip()
             finalGrade = gradeSoup[i].contents[15].contents[0].strip()
             GPA = gradeSoup[i].contents[16].contents[0].strip()
-            x.add_row(
+            courseTable.add_row(
                 [term, courseCode, courseName, courseKind, courseCredit, firstGrade, secondGrade, finalGrade, GPA])
-        print(x)
+        print(courseTable)
 
     def creditCount(self, gradeSoup):
         total = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         for i in range(1, len(gradeSoup)):
             kind = gradeSoup[i].contents[3].contents[0].strip()[0]
             if kind in classKind.keys():
+                if gradeSoup[i].contents[16].contents[0].strip() == '0':
+                    continue
                 total[classKind[kind]] += float(gradeSoup[i].contents[11].contents[0].strip())
             else:
                 total[12] += float(gradeSoup[i].contents[11].contents[0].strip())
-        x, y = creatTable(total)
+        x, y = getCreditTable(total)
         print('{}\n{}'.format(x, y))
         for i in range(len(sumAll)):
             sumAll[i] += total[i]
@@ -113,55 +112,97 @@ class student(object):
 sumAll = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 
-def creatTable(total):
-    x = PrettyTable(['种类', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'X', '总和'])
-    x.add_row(['已修', total[0], total[1], total[2], total[3], total[4], total[5], total[6], total[7],
-               total[8], total[9], total[10], total[11], total[12], sum(total)])
-    y = PrettyTable(['小类', 'A', 'B', 'C', 'D', 'E + F + G', 'H + I + J', 'K + L', 'X', '总和'])
-    y.add_row(['已修合计', total[0], total[1], total[2], total[3], (sum(total[4:6])),
-               (sum(total[7:9])), (sum(total[10:11])), total[12], sum(total)])
-    return x, y
+def getCreditTable(total):
+    detailTable = PrettyTable(['种类', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'X', '总和'])
+    detailTable.add_row(['已修', total[0], total[1], total[2], total[3], total[4], total[5], total[6], total[7],
+                         total[8], total[9], total[10], total[11], total[12], sum(total)])
+    classifyTable = PrettyTable(['小类', 'A', 'B', 'C', 'D', 'E + F + G', 'H + I + J', 'K + L', 'X', '总和'])
+    classifyTable.add_row(['已修合计', total[0], total[1], total[2], total[3], (sum(total[4:6])),
+                           (sum(total[7:9])), (sum(total[10:11])), total[12], sum(total)])
+    return detailTable, classifyTable
 
 
-def main():
-    print('***说明：供成电理工类专业查询学分，推荐全屏使用***\n')
-    for i in sumAll:
-        i = 0
+def getCreditDescribeTable():
+    describeTable = PrettyTable(['课程代码', '说明', '所需学分'])
+    describeTable.add_row(['A', '核心通识', '4'])
+    describeTable.add_row(['B', '基础通识', '32'])
+    describeTable.add_row(['C', '交叉通识', '3'])
+    describeTable.add_row(['D', '学科通识', '27'])
+    describeTable.add_row(['E + F + G', '学科基础+学科拓展+专业核心', '50'])
+    describeTable.add_row(['H + I + J', '专业选修+素质教育+创新拓展', '29'])
+    describeTable.add_row(['K + L', '实验课程+实习实训', '25'])
+    describeTable.add_row(['X', '未知类别', ''])
+    describeTable.add_row(['', '毕业共需', '170'])
+    return describeTable
+
+
+def getUserInfo():
     try:
-        from password import username, password
+        with open('loginInfo.json', 'r') as f:
+            userinfo = json.load(f)
+            username = userinfo['username']
+            password = userinfo['password']
+            ifUse = input('查询到本地有保存的学号密码文件，是否使用（N:不使用，默认使用）:')
+            if ifUse == 'N' or ifUse == 'n':
+                raise None
     except Exception as e:
         username = input('输入学号：')
         password = input('信息门户密码：')
+        ifStore = input('是否储存学号及密码到本地，明文储存非信任的电脑勿选（Y:存，默认不存）：')
+        if ifStore == 'Y' or ifStore == 'y':
+            with open('loginInfo.json', 'w') as f:
+                userinfo = json.dumps({'username': username, 'password': password}, sort_keys=True, indent=4)
+                f.write(userinfo)
+    return username, password
+
+
+def main():
+    for i in sumAll:
+        i = 0
+    username, password = getUserInfo()
     s = student(username, password)
-    term = 6
-    term = int(input('将查询前N个学期的学分(请勿大于所学学期总数)：'))
+    print('登陆成功！\n')
+    term = int(input('将查询前N个学期的学分(请勿大于所学学期总数),输入N='))
     for i in range(term):
         s.getGrade(i + 1)
         # s.allGrade()
-    x, y = creatTable(sumAll)
+    detailTable, classifyTable = getCreditTable(sumAll)
 
-    y.add_row(['毕业共需', 4.0, 32.0, 3.0, 27.0, 50.0, 29.0, 25.0, 0.0, 170.0])
-    y.add_row(['还需', 4.0 - sumAll[0], 32.0 - sumAll[1], 3.0 - sumAll[2], 27.0 - sumAll[3], 50.0 - sum(sumAll[4:6]),
-               29.0 - sum(sumAll[7:9]), 25.0 - sum(sumAll[10:11]), 0.0 - sumAll[12], 170.0 - sum(sumAll)])
+    classifyTable.add_row(['毕业共需', 4.0, 32.0, 3.0, 27.0, 50.0, 29.0, 25.0, 0.0, 170.0])
+    classifyTable.add_row(['还需', 4.0 - sumAll[0], 32.0 - sumAll[1], 3.0 - sumAll[2], 27.0 - sumAll[3],
+                           50.0 - sum(sumAll[4:6]), 29.0 - sum(sumAll[7:9]), 25.0 - sum(sumAll[10:11]),
+                           0.0 - sumAll[12], 170.0 - sum(sumAll)])
 
-    z = PrettyTable(['课程代码', '说明', '所需学分'])
-    z.add_row(['A', '核心通识', '4'])
-    z.add_row(['B', '基础通识', '32'])
-    z.add_row(['C', '交叉通识', '3'])
-    z.add_row(['D', '学科通识', '27'])
-    z.add_row(['E + F + G', '学科基础+学科拓展+专业核心', '50'])
-    z.add_row(['H + I + J', '专业选修+素质教育+创新拓展', '29'])
-    z.add_row(['K + L', '实验课程+实习实训', '25'])
-    z.add_row(['X', '未知类别', ''])
-    z.add_row(['', '毕业共需', '170'])
-    print('\n\n前{}学期合计统计如下:\n{}\n{}\n\n说明:\n{}'.format(term, x, y, z))
+    describeTable = getCreditDescribeTable()
+
+    print('\n\n前{}学期合计统计如下:\n{}\n{}\n\n说明:\n{}'.format(term, detailTable, classifyTable, describeTable))
     print('数据来源于教务系统，仅供参考使用，以培养方案为准！！！\n\n')
 
 
+def checkNew():
+    version = '0.1.1'
+    import urllib.request
+    latestURL = 'https://api.github.com/repos/Cheain/creditAnalysis-UESTC/releases/latest'
+    latestInfo = urllib.request.urlopen(latestURL).read().decode(encoding='utf-8')
+    latestInfo = json.loads(latestInfo)
+    if latestInfo['tag_name'] <= version:
+        print('***当前版本为最新版,请继续使用***\n')
+    else:
+        tag = input('当前版本不是最新版，是否更新（N:不更新，默认更新）：')
+        if tag != 'N' and tag != 'n':
+            urllib.request.urlretrieve(latestInfo['assets'][0]['browser_download_url'],
+                                       'main-{}.exe'.format(latestInfo['tag_name']))
+            print('***已下载最新版，请重启***\n')
+            os.system('pause')
+            exit()
+
+
 if __name__ == '__main__':
+    print('***说明：供成电理工类专业查询学分，推荐全屏使用***\n')
     try:
+        checkNew()
         main()
     except Exception as e:
-        print('出错：{}\n程序异常请联系https://github.com/Cheain/creditAnalysis-UESTC/issues'.format(e))
+        print('出错:{}\n程序异常请联系:https://github.com/Cheain/creditAnalysis-UESTC/issues'.format(e))
     finally:
         os.system('pause')
